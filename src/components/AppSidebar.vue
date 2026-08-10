fd56d0b48354c3d12c60ac1a22c2a1e31420a547
richie <280645618@qq.com>
2026-08-10 09:24:12 +0800

    完善左侧菜单

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -69,12 +69,6 @@ const navItems = [
     label: '提醒',
     icon: '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/></svg>'
   },
-  {
-    path: '/tools',
-    label: '工具箱',
-    beta: true,
-    icon: '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/></svg>'
-  },
 ];
 
 const isActive = (path: string) => {
